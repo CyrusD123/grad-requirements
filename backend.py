e51964d0e9d611d3c33298fda11fbf86180a50de
CyrusD123 <pseudonym112304@gmai.com>
2020-11-21 12:29:47 -0500

    added Heroku Postgres

--- backend.py
+++ backend.py
@@ -4,6 +4,13 @@ import psycopg2
 
 app = Flask(__name__)
 
+# Connect to database using heroku environment variable DATABASE_URL
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')# Heroku requires SSL
+
+# Create a cursor to execute queries
+cursor = conn.cursor()
+
 
 @app.route('/')
 def hello():
@@ -11,3 +18,5 @@ def hello():
 
 if __name__ == '__main__':
     app.run()
+    cursor.close()
+    conn.close()
